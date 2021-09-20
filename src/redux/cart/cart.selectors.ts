b4e567d5cb655c85593aec5df644bb79a26956ee
unknown <ttyt0416@gmail.com>
2021-09-20 17:11:02 +0900

    adding informations and datas for redux

---
+++ src/redux/cart/cart.selectors.ts
@@ -0,0 +1,31 @@
+import { createSelector } from "reselect";
+
+const selectCart = (state: any) => state.cart;
+
+export const selectCartItems = createSelector(
+  [selectCart],
+  (cart) => cart.cartItems
+);
+
+export const selectCartHidden = createSelector(
+  [selectCart],
+  (cart) => cart.hidden
+);
+
+export const selectCartItemsCount = createSelector(
+  [selectCartItems],
+  (cartItems) =>
+    cartItems.reduce(
+      (accumalatedQuantity: any, cartItem: any) =>
+        accumalatedQuantity + cartItem.quantity,
+      0
+    )
+);
+
+export const selectCartTotal = createSelector([selectCartItems], (cartItems) =>
+  cartItems.reduce(
+    (accumalatedQuantity: any, cartItem: any) =>
+      accumalatedQuantity + cartItem.quantity * cartItem.price,
+    0
+  )
+);
